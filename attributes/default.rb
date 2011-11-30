bbd4021b8e24c06e21b018f31e4911c33505ac35
Ian Formanek <ian.formanek@gmail.com>
2011-11-30 15:53:17 +0100

    Updated splunk version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default[:splunk][:root] = "/opt/splunk"
-default[:splunk][:version] = "4.2.1-98164"
+default[:splunk][:version] = "4.2.4-110225"
 default[:splunk][:proxy_user] = "admin"
 default[:splunk][:proxy_pass] = "{SHA}+pvrmeQCmtWmYVOZ57uuITVghrM="
 default[:splunk][:alt_htpasswd] = false
